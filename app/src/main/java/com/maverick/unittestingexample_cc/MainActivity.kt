7214fcf6f91288f32af8c3efb1ff03e3d014d51a
Nikunj <contact@yamiitsolutions.com>
2023-01-25 10:24:08 +0530

    6.2 Espresso Unit Test
     * Quotify Sources Added

--- app/src/main/java/com/maverick/unittestingexample_cc/MainActivity.kt
+++ app/src/main/java/com/maverick/unittestingexample_cc/MainActivity.kt
@@ -1,18 +1,58 @@
 package com.maverick.unittestingexample_cc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.maverick.unittestingexample_cc.databinding.ActivityMainBinding
+import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var mainViewModel: MainViewModel
+
+    private val quoteText: TextView
+        get() = findViewById(R.id.quoteText)
+
+    private val quoteAuthor: TextView
+        get() = findViewById(R.id.quoteAuthor)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        mainViewModel = ViewModelProvider(
+            this,
+            MainViewModelFactory(application)
+        ).get(MainViewModel::class.java)
+        setQuote(mainViewModel.getQuote())
+
+    }
+
+    fun setQuote(quote: Quote) {
+        quoteText.text = quote.text
+        quoteAuthor.text = quote.author
+    }
+
+    fun onPrevious(view: View) {
+        runBlocking {
+            setQuote(mainViewModel.previousQuote())
+        }
+    }
+
+    fun onNext(view: View) {
+        runBlocking {
+            setQuote(mainViewModel.nextQuote())
+        }
+    }
+
+    fun onShare(view: View) {
+        runBlocking {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.setType("text/plain")
+            intent.putExtra(Intent.EXTRA_TEXT, mainViewModel.getQuote().text)
+            startActivity(intent)
+        }
     }
 
 }
